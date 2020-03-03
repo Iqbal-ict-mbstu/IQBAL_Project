afce077f70ae8155d89c3d7e4522e5ad44495674
Iqbal-ict-mbstu <iqbalhossen1563@gmail.com>
2020-03-03 10:36:34 +0600

    First comment

---
+++ src/javaapplat/First.java
@@ -0,0 +1,11 @@
+
+package javaapplat;
+
+import java.applet.Applet;  
+import java.awt.Graphics;  
+public class First extends Applet {
+    public void paint(Graphics g){  
+g.drawString("welcome",15,150);  
+}  
+    
+}
---
+++ src/javaapplat/JavaApplat.java
@@ -0,0 +1,15 @@
+
+package javaapplat;
+import java.applet.Applet;  
+import java.awt.Graphics;  
+
+public class JavaApplat extends Applet {
+    public void paint(Graphics g){
+        g.drawString("Iqbal Hossen ", 45, 50);
+    }
+
+   
+    
+   
+    
+}
